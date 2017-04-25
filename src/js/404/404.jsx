e67d943a9462a87e95d3a47ca95db5406713e0ac
shenchenkai <shenck@joyowo.com>
2017-04-25 16:39:44 +0800

    css module

---
+++ src/js/404/404.jsx
@@ -0,0 +1,20 @@
+import React,{
+    Component,
+} from 'react';
+
+
+class NotFoundPageApp extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+            <div style={{color: '#fff'}}>
+                404
+            </div>
+        );
+    }
+}
+
+export default NotFoundPageApp;
